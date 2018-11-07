cbf9cc5863e266d8155773d05f83dc0b4fde75e8
shureim <tarickaliabdi@gmail.com>
2018-11-07 12:26:47 +0300

    added the welcome function to test the HttpResponse in views.py

--- insta/views.py
+++ insta/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
-# from django.http  import HttpResponse
-#
-# # Create your views here.
-# def welcome(request):
-#     return render(request,'welcome.html')
-#
+from django.shortcuts import render
+from django.http  import HttpResponse
+
+# Create your views here.
+def welcome(request):
+    return render(request,'welcome.html')
+
 # def today(request):
 #     return render(request,'all-insta/insta.html')
