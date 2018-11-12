467043b59fb0085a4af77fcb7c8d6c43186313aa
shureim <tarickaliabdi@gmail.com>
2018-11-12 10:44:38 +0300

    added delete function for  class profile in the models.py

--- insta/models.py
+++ insta/models.py
@@ -11,9 +11,9 @@ class Profile(models.Model):
 
     def save_profile(self):
         self.save()
-#
-#     def delete_profile(self):
-#         self.delete()
+
+    def delete_profile(self):
+        self.delete()
 #
 #     @classmethod
 #     def update_profile(cls,profile,update):
