315ac57ac3b6723561cc0eb41f3a65eb83cefd52
shureim <tarickaliabdi@gmail.com>
2018-11-12 10:43:54 +0300

    added class profile in the models.py

--- insta/models.py
+++ insta/models.py
@@ -1,14 +1,14 @@
-# from django.db import models
-# import datetime as dt
-# from django.contrib.auth.models import User
-# from tinymce.models import HTMLField
-#
-# # Create your models here.
-# class Profile(models.Model):
-#     profile_photo = models.ImageField(upload_to = 'images/', blank = True)
-#     user = models.ForeignKey(User,on_delete = models.CASCADE)
-#     bio = models.TextField(max_length = 100)
-#
+from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
+# Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'images/', blank = True)
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    bio = models.TextField(max_length = 100)
+
 #     def save_profile(self):
 #         self.save()
 #
