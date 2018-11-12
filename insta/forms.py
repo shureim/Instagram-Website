74d92db7e0f5873a60730d0c56eb2d669c4f930c
shureim <tarickaliabdi@gmail.com>
2018-11-12 10:41:48 +0300

    added form for user to add image in the form.py

--- insta/forms.py
+++ insta/forms.py
@@ -1,11 +1,11 @@
-# from django import forms
-# from .models import Image,Profile,Comment
-#
-# class NewImageForm(forms.ModelForm):
-#     class Meta:
-#         model = Image
-#         exclude = ['profile', 'photo_date','likes','comment','image_name']
-#
+from django import forms
+from .models import Image,Profile,Comment
+
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['profile', 'photo_date','likes','comment','image_name']
+
 # class NewProfileForm(forms.ModelForm):
 #     class Meta:
 #         model = Profile
